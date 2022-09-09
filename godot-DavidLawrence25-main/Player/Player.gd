7599f4151044dd8684bc4dce0ac4853219d755ed
DavidLawrence25 <97046243+DavidLawrence25@users.noreply.github.com>
2022-09-09 08:57:58 -0600

    Ended at T3-391
    https://www.youtube.com/watch?v=TQKXU7iSWUU&list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a&index=3

--- godot-DavidLawrence25-main/Player/Player.gd
+++ godot-DavidLawrence25-main/Player/Player.gd
@@ -1,15 +1,28 @@
+# extensions
 extends KinematicBody2D
 
+# constants
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+
+# variables
 var velocity = Vector2.ZERO
 
+# player physics
 func _physics_process(delta):
+	# input handler
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	# normalizes input
+	input_vector = input_vector.normalized()
 	
+	# converts input to velocity
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	move_and_collide(velocity)
+	# cheaty movement function
+	move_and_collide(velocity * delta)
