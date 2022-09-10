d0d99af685e3e760d6ae545d797615418529c0c5
DavidLawrence25 <97046243+DavidLawrence25@users.noreply.github.com>
2022-09-10 16:20:45 -0600

    Completed Tutorial 3

--- godot-DavidLawrence25-main/Player/Player.gd
+++ godot-DavidLawrence25-main/Player/Player.gd
@@ -25,4 +25,4 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	# cheaty movement function
-	move_and_collide(velocity * delta)
+	velocity = move_and_slide(velocity)
