446744111d0dad2b1edded6ddb312c75e27ab2c3
DavidLawrence25 <97046243+DavidLawrence25@users.noreply.github.com>
2022-09-10 18:28:50 -0600

    Completed Tutorial 5

--- godot-DavidLawrence25-main/Player/Player.gd
+++ godot-DavidLawrence25-main/Player/Player.gd
@@ -8,6 +8,7 @@ const FRICTION = 500
 
 # variables
 var velocity = Vector2.ZERO
+onready var animationPlayer = $AnimationPlayer
 
 # player physics
 func _physics_process(delta):
@@ -20,8 +21,14 @@ func _physics_process(delta):
 	
 	# converts input to velocity
 	if input_vector != Vector2.ZERO:
+		# disgusting if statement that will go bye-bye with an animation tree later
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	# cheaty movement function
