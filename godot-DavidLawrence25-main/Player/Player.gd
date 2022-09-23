30f981ed5c2a363e7c28b0caec4d864fb87fa9ec
DavidLawrence25 <97046243+DavidLawrence25@users.noreply.github.com>
2022-09-23 16:29:42 -0600

    Completed Tutorial 9

--- godot-DavidLawrence25-main/Player/Player.gd
+++ godot-DavidLawrence25-main/Player/Player.gd
@@ -6,14 +6,35 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+# enumerations
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
 # variables
+var state = MOVE
 var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
-# player physics
+func _ready():
+	animationTree.active = true
+
+# main stuff
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			roll_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+# state machines
+func move_state(delta):
 	# input handler
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -25,6 +46,7 @@ func _physics_process(delta):
 		# animate
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		# converts input to velocity
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
@@ -36,3 +58,17 @@ func _physics_process(delta):
 	
 	# cheaty movement function
 	velocity = move_and_slide(velocity)
+	
+	# check if attack is pressed
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
+
+func roll_state(delta):
+	pass
